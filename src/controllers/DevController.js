61b7f8b36ae69866ed859f1af72c87941758af1a
Victor Huggo <victorhuggo97@gmail.com>
2020-01-15 14:01:38 -0300

    commit

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,7 +1,14 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
+    async index(request, response){
+        const devs = await Dev.find();
+
+        return response.json(devs);
+    },
+
     async store (request, response) {
         const{ github_username, techs, latitude, longitude } = request.body;
 
@@ -12,7 +19,7 @@ module.exports = {
     
             const { name = login, avatar_url, bio } = apiResponse.data;
         
-            const techsArray = techs.split(',').map(tech => tech.trim());
+            const techsArray = parseStringAsArray(techs);
         
             const location =  {
                 type: 'Point',

4204070cd84b173a99f9d8567bdd6b926345b89e
Victor Huggo <victorhuggo97@gmail.com>
2020-01-17 17:13:39 -0300

    commit

--- src/websocket.js
+++ src/websocket.js
@@ -1,10 +1,13 @@
 const socketio = require('socket.io');
 const parseStringAsArray = require('./utils/parseStringAsArray');
+const calculateDistance = require('./utils/calculateDistance');
 
+
+let io;
 const connection = [];
 
 exports.setupWebsocket = (server) => {
-    const io = socketio(server);
+    io = socketio(server);
 
     io.on('connection', socket => {
         const { latitude, longitude, techs} = (socket.handshake.query);
@@ -20,8 +23,16 @@ exports.setupWebsocket = (server) => {
     });
 };
 
+
 exports.findConnections = (coordinates, techs) => {
     return connections.filter(connection => {
-        return 
+        return calculateDistance(coordinates, connection.coordinates) < 10
+            && connection.techs.some(item => techs.includes(item))
+    })
+}
+
+exports.sendMessage = (to, message, data) => {
+    to.forEach(connection => {
+        io.to(connection.id).emit(message, data);
     })
 }
